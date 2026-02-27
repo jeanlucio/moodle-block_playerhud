b62c873f6a6615faa695c4b56eb0e9be06d940e4
Jean Lúcio <jeanlucio@gmail.com>
2026-02-27 00:28:11 +0000

    Release: Bump version to v1.0.5

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_playerhud';
-$plugin->version   = 2026021804;
+$plugin->version   = 2026022600;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2024100700;        // Requires: Moodle 4.5+.
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v1.0.4';
+$plugin->release   = 'v1.0.5';
